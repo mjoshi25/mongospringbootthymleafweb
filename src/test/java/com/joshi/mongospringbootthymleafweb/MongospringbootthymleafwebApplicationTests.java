1c06a2879441f08f7133e53313c5006c467cb6fd
mjoshi25 <manabhanjan22@gmail.com>
2023-01-22 17:43:50 +0530

    Initial commit

---
+++ src/test/java/com/joshi/mongospringbootthymleafweb/MongospringbootthymleafwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshi.mongospringbootthymleafweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongospringbootthymleafwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
